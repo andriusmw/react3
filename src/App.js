2326aeab916844f62fd4c2a42a481c3d7d0db976
andriusmw <andriusmw@yahoo.es>
2022-03-22 16:27:25 +0100

    cargando peliculas desde el array, luego será desde json

--- src/App.js
+++ src/App.js
@@ -4,18 +4,48 @@ import Pelicula from './pelicula';
 import PageWrapper from './PageWrapper';
 
 function App() {
+
+	let peliculas =[
+		{
+			titulo:"Oblivion (2012)",
+			calificacion:"8.1",
+			director:"Joss Whedon",
+			actores:"Robert Downey Jr.,Chris Evans,Chris Hemsworth" ,
+			fecha:"1 May 2015",
+			duracion:"2h 21min",  
+			img:"images/uploads/mv1.jpg",
+			descripcion:"Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...",
+		},
+		{
+			titulo:"Into the Wild 2014",
+			calificacion:"7.8",
+			director:"Joss Whedon",
+			actores:"Robert Downey Jr.,Chris Evans,Chris Hemsworth" ,
+			fecha:"1 May 2015",
+			duracion:"2h 21min",  
+			img:"images/uploads/mv2.jpg",
+			descripcion:"Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...",
+		}
+	]
   
   return (
 
 	<PageWrapper>
 
-			<Pelicula  titulo="Oblivion (2012)" calificacion="8.1" 
-          	  director="Joss Whedon" actores="Robert Downey Jr.,Chris Evans,Chris Hemsworth" 
-           		 fecha="1 May 2015" duracion="2h 21min"  img="images/uploads/mv1.jpg">
-                    Earth's mightiest heroes must come together and learn to fight as a team 
-                    if they are to stop the mischievous Loki and his alien army from enslaving humanity...
+			{peliculas.map(pelicula =>{
+				return	<Pelicula  
+			     	       titulo={pelicula.titulo} 
+						   calificacion={pelicula.calificacion} 
+          	 			   director={pelicula.director} 
+						   actores={pelicula.actores} 
+           			       fecha={pelicula.fecha} 
+					   	   duracion={pelicula.duracion} 
+						   img={pelicula.img}
+                           descripcion={pelicula.descripcion}>
     
-            </Pelicula>	
+           			    </Pelicula>	
+		    })}	
+			
 
 	</PageWrapper>
   ) 
