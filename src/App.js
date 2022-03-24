a1ee5df401a70407257f862847a2cc86f8b74382
andriusmw <andriusmw@yahoo.es>
2022-03-24 12:22:34 +0100

    paginacion.js funciona

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Pelicula from './pelicula';
 import PageWrapper from './PageWrapper';
 import PeliculasJson from './peliculas.json';
+import Paginacion from './paginacion';
 //Este es el array previo de películas desde donde cargamos el listado
 //de pelis con la estructura del objeto película para cada elemento del 
 //array y asi podemos pedirle el título, fecha etc de cada objeto(pelicula)
@@ -28,6 +29,8 @@ function App() {
     
            			    </Pelicula>	
 		    })}	
+
+			<Paginacion ></Paginacion>
 			
 
 	</PageWrapper>
